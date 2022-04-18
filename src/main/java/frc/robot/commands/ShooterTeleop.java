58df5dda95d6e5cc2cb95233af87f8d83617a227
Anish Gangu <56240492+AG-Coding@users.noreply.github.com>
2022-04-18 00:53:40 -0700

    Rewrote Execute

--- src/main/java/frc/robot/commands/ShooterTeleop.java
+++ src/main/java/frc/robot/commands/ShooterTeleop.java
@@ -1,40 +1,39 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.VisionSubsystem;
 
 public class ShooterTeleop extends CommandBase {
-    private VisionSubsystem m_vision;
     private ShooterSubsystem m_shooter;
     private IndexerSubsystem m_indexer;
     private DriveSubsystem m_drive;
 
-    public ShooterTeleop(VisionSubsystem vision, ShooterSubsystem shooter, IndexerSubsystem indexer, DriveSubsystem drive) {
-        m_vision = vision;
+    public ShooterTeleop(ShooterSubsystem shooter, IndexerSubsystem indexer, DriveSubsystem drive) {
         m_shooter = shooter;
         m_indexer = indexer;
         m_drive = drive;
-        addRequirements(m_vision);
         addRequirements(m_shooter);
         addRequirements(m_drive);
         addRequirements(m_indexer);
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        m_indexer.retract();
+    }
 
     @Override
     public void execute() {
-        m_drive.updateSpeed(0, 0, m_vision.steeringAssist(), false);
-        m_shooter.shoot(m_shooter.getClampedRPM());
-        new WaitCommand(1);
-        m_drive.updateSpeed(0, 0, 0, false);
-        m_indexer.extend();
-        new WaitCommand(1);
+            new RunCommand(m_drive::updateSpeedAuto, m_drive)
+            .alongWith(new RunCommand(m_shooter::shootAuto, m_shooter))
+            .withTimeout(1.5)
+            .andThen(m_drive::stop, m_drive)
+            .andThen(m_indexer::extend, m_indexer)
+            .andThen(new WaitCommand(1));
     }
 
     public void end() {
