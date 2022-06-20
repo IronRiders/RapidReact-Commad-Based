86046edb73c616082f4aec8805cfd1d6403ee2c4
Anish Gangu <56240492+AG-Coding@users.noreply.github.com>
2022-06-20 14:56:42 -0700

    Formatted the code

--- src/main/java/frc/robot/commands/MecanumPathFollower.java
+++ src/main/java/frc/robot/commands/MecanumPathFollower.java
@@ -8,10 +8,10 @@ import frc.robot.subsystems.MecanumWheel;
 public class MecanumPathFollower extends PPMecanumControllerCommand {
 
     public MecanumPathFollower(PathPlannerTrajectory trajectory, DriveSubsystem drive) {
-       super(trajectory, 
-       () -> drive.getPose2d(), 
-       drive.getKinematics(), 
-       drive.getxController(), drive.getyController(), drive.getThetaController(), 
-       MecanumWheel.getMaxLinearVelocity(), (speed) -> drive.SetWheelSpeeds(speed), drive);
+        super(trajectory,
+                () -> drive.getPose2d(),
+                drive.getKinematics(),
+                drive.getxController(), drive.getyController(), drive.getThetaController(),
+                MecanumWheel.getMaxLinearVelocity(), (speed) -> drive.SetWheelSpeeds(speed), drive);
     }
 }
